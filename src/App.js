1ffe36c35fb130a3657923ea49d6145110aee0bf
Victoria <95590803+anarayner@users.noreply.github.com>
2022-04-22 19:30:30 -0400

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Button, Container} from '@mui/material';
+import {useDispatch, useSelector} from 'react-redux';
+import {addCustomerAction, removeCustomerAction} from './store/customerReducer';
+import {fetchCustomers} from './store/asyncActions/customers';
 
-function App() {
+const App = () => {
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash.cash)
+    const customers = useSelector(state => state.customers.customers)
+
+    const add = () => {
+      dispatch({type: 'ADD_CASH', payload: 5})
+    }
+    const get = () => {
+        dispatch({type: 'GET_CASH', payload: 5})
+    }
+    const removeCustomer = (customer) => {
+        dispatch(removeCustomerAction(customer.id))
+    }
+    const addCustomer = (name) => {
+        const customer = {
+            name,
+            id: Date.now()
+        }
+        dispatch(addCustomerAction(customer))
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Container style={{fontSize: '35px', marginTop: '20px'}}>
+          {cash}
+          <Button style={{margin: '20px'}}
+                  onClick={() => add()} variant="contained">Add cash</Button>
+          <Button onClick={() => get()} variant="contained">Get cash</Button>
+              <Button style={{margin: '20px'}}
+                      onClick={() => addCustomer(prompt())} variant="contained">Add user</Button>
+              <Button onClick={() => get()} variant="contained">Remove user</Button>
+              <Button onClick={() => dispatch(fetchCustomers())} variant="contained">Fetch users from db</Button>
+
+              {customers.length > 0?
+              <div>
+                  {customers.map(customer =>
+                      <div key={customer.id}
+                           style={{cursor: 'default'}}
+                           onClick={()=>removeCustomer(customer)}
+                      >{customer.name}</div>
+                  )}
+              </div>
+                  :
+              <div style={{fontSize: '35px', marginTop: '20px'}}>No customers</div>
+
+              }
+          </Container>
+      </div>
   );
-}
+};
 
 export default App;
+
+
